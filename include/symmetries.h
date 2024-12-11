de681db53d129f9c282c029bf8befa1564648170
Johannes Hauschild <git@johannes-hauschild.de>
2024-12-11 21:35:33 +0100

    remove SectorArray class, represent Sector by a single int instead
    implement compress_Sector and decompress_Sector

--- include/symmetries.h
+++ include/symmetries.h
@@ -1,6 +1,12 @@
 #pragma once
 
 #include <vector>
+#include <cstdint>
+#include <string>
+#include <array>
+
+#include <stdexcept>
+#include <cassert>
 
 namespace cyten {
 
@@ -17,25 +23,24 @@ enum class BraidingStyle {
     no_braiding = 30,  // braiding is not defined
 };
 
-typedef signed int charge;
 
-typedef std::vector<charge> Sector;
+// representation of a Sector by a single 64-bit int
+// Product symmetries limit the size of each of the Sectors to a fixed bit-length such that total bit-length <= 64.
+typedef int64_t Sector;
 
 
-/// @brief vector of fixed-length Sectors given during intialization
-class SectorArray {
-    private:
-        std::vector<charge> data; 
-    public:
-        const size_t sector_len;
-        SectorArray(size_t sector_ind_len);
-        size_t size() const;
-        Sector operator[](size_t i);
-        const Sector & operator[](size_t i) const; 
-        void push_back(Sector const & s);
-        charge * raw_pointer();
-};
+Sector compress_Sector_dynamic(std::vector<Sector> decompressed, std::vector<int> const & bit_lengths);
 
+template<int N>
+Sector compress_Sector(std::array<Sector, N> decompressed, std::array<int, N> const & bit_lengths);
+
+std::vector<Sector> decompress_Sector_dynamic(Sector compressed, std::vector<int> const & bit_lengths);
+
+template<int N>
+std::array<Sector, N> decompress_Sector(Sector compressed, std::array<int, N> const & bit_lengths);
+
+
+typedef std::vector<Sector> SectorArray;
 
 class Symmetry {
     public:
@@ -56,5 +61,51 @@ class U1Symmetry: public Symmetry {
 //     const std::vector<Symmetry const *> factors;
 // }
 
+
+template<int N>
+Sector compress_Sector(std::array<Sector, N> decompressed, std::array<int, N> const & bit_lengths)
+{
+    Sector compressed = 0;
+    int shift = 0;
+    const Sector sign_bit = Sector(1) << 63;
+    for (int i = 0; i < N ; ++i) 
+    {
+        //keep least significant bit_lengths[i] bits
+        // note: negative values start with bitstrings of 1, so most significant bit kept is new sign bit.
+        Sector mask_last_bits = (Sector(1) << bit_lengths[i]) - 1;
+        Sector compress_i = decompressed[i] & mask_last_bits;
+        if (decompressed[i] >> 63 & 1) {
+            if ((decompressed[i] | ((Sector(1) << (bit_lengths[i] - 1)) - 1)) != Sector(-1))
+                throw std::overflow_error("discarding bits in compress_Sector");
+        } else {
+            if ((decompressed[i] & ~((Sector(1) << (bit_lengths[i] - 1)) - 1)) != Sector(0))
+                throw std::overflow_error("discarding bits in compress_Sector");
+        }
+        compressed |= compress_i << shift;
+        shift += bit_lengths[i];           
+    }
+    assert(shift <= 64);
+    return compressed;
+}
+
+template<int N>
+std::array<Sector, N> decompress_Sector(Sector compressed, std::array<int, N> const & bit_lengths)
+{
+    std::array<Sector, N> decompressed;
+    int shift = 0;
+    const Sector sign_bit = Sector(1) << 63;
+    for (size_t i = 0; i < N ; ++i) 
+    {
+        // kept sign bit + bit_lengths[i]-1 bits from right of decompressed[i]
+        Sector mask_last_bits = (Sector(1) << bit_lengths[i]) - 1;
+        Sector decomp = (compressed >> shift) & mask_last_bits;
+        if (decomp >> (bit_lengths[i] - 1))
+            decomp |= ~mask_last_bits;
+        decompressed[i] = decomp;
+        shift += bit_lengths[i];           
+    }
+    assert(shift <= 64);
+    return decompressed;
+}
 
 }
