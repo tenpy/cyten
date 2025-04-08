b5d7997ecb97cf0712ac17f35d81dbc3ccc7238b
nikirchner <126802899+nikirchner@users.noreply.github.com>
2025-04-08 16:37:45 +0200

    fix default values for make_pipe

--- cyten/backends/no_symmetry.py
+++ cyten/backends/no_symmetry.py
@@ -65,7 +65,7 @@ class NoSymmetryBackend(TensorBackend):
         assert self.block_backend.sum_all(a.data) == a.small_leg.dim
 
     def make_pipe(self, legs: list[Leg], is_dual: bool, in_domain: bool,
-                  pipe: LegPipe | None) -> LegPipe:
+                  pipe: LegPipe | None = None) -> LegPipe:
         assert all(isinstance(l, ElementarySpace) for l in legs)  # OPTIMIZE rm check
         if isinstance(pipe, AbelianLegPipe):
             assert pipe.combine_cstyle == (not is_dual)
