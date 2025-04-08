7d6dc4fc186caa3c94900ae1fe9724ca207d5ba7
nikirchner <126802899+nikirchner@users.noreply.github.com>
2025-04-08 16:37:45 +0200

    fix default values for make_pipe

--- cyten/backends/abelian.py
+++ cyten/backends/abelian.py
@@ -251,7 +251,7 @@ class AbelianBackend(TensorBackend):
     # OVERRIDES
 
     def make_pipe(self, legs: list[Leg], is_dual: bool, in_domain: bool,
-                  pipe: LegPipe | None) -> LegPipe:
+                  pipe: LegPipe | None = None) -> LegPipe:
         assert all(isinstance(l, ElementarySpace) for l in legs)  # OPTIMIZE rm check
         if isinstance(pipe, AbelianLegPipe):
             assert pipe.combine_cstyle == (not is_dual)
