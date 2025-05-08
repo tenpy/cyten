edac83d20d12c6af73af235e06598bd659b8e640
Jakob Unfried <jakob.unfried@tum.de>
2025-05-08 16:36:51 +0200

    Add SparseMapping

---
+++ cyten/tools/mappings.py
@@ -0,0 +1,64 @@
+"""Tools for the sparse mappings that ocurr when manipulating fusion trees."""
+# Copyright (C) TeNPy Developers, Apache license
+
+from __future__ import annotations
+from typing import TypeVar, TypeAlias, Generic, Iterable
+
+
+__all__ = ['SparseMapping']
+
+
+_KT = TypeVar('_KT')  # type for keys, labelling basis elements
+_Scalar: TypeAlias = float | complex  # type for "entries" of a SparseMapping
+
+
+class SparseMapping(Generic[_KT], dict[_KT, dict[_KT, _Scalar]]):
+    r"""A sparse matrix, where the labels of basis states are a structured type, not just int.
+
+    Used in :class:`cyten.backends.fusion_tree_backend.TreePairMapping` and related objects.
+
+    To represent the mapping ``e_j -> \sum_i A_{ij} e_i``, we store ``self[j][i] = A_{ij}``.
+    I.e. a single entry ``self[j][i] = a`` represents the contribution ``e_j -> a e_i``.
+    """
+
+    @classmethod
+    def from_identity(cls, keys: Iterable[_KT]):
+        res = cls()
+        for i in keys:
+            res[i] = {i: 1}
+        return res
+
+    def precompose(self, other: SparseMapping[_KT] | dict[_KT, dict[_KT, _Scalar]]
+                   ) -> SparseMapping[_KT]:
+        r"""The composite ``res_{ik} = \sum_j other_{ij} self{jk}``, such that self acts first.
+
+        I.e. we precompose self with other, i.e. compose other with self, i.e.::
+
+            precompose(self, other) : x ↦ other(self(x)) = (other ∘ self)(x)
+        """
+        # e_k -> \sum_j self_{jk} e_j -> \sum_j self_{jk} \sum_i other_{ij} e_i
+        # res_{ik} = \sum_j other_{ij} self_{jk}
+        # res[k][i] = \sum_j other[j][i] * self[k][j]
+        res = SparseMapping()
+        for k, self_k in self.items():
+            res[k] = res_k = {}
+            for j, self_jk in self_k.items():
+                if j not in other:
+                    continue
+                for i, other_ij in other[j].items():
+                    res_k[i] = res_k.get(i, 0) + other_ij * self_jk
+        return res
+
+    def nonzero_rows(self) -> set[_KT]:
+        """The idcs ``i`` for which there are entries ``self_{ij} = self[j][i]`` set."""
+        return set(i for self_j in self.values() for i in self_j.keys())
+
+    def nonzero_cols(self) -> set[_KT]:
+        """The idcs ``j`` for which there are entries ``self_{ij} = self[j][i]`` set."""
+        return set(self.keys())
+
+    def prune(self, cutoff: float):
+        """Remove small entries, in-place."""
+        for j in self.keys():
+            self[j] = {i: a for i, a in self[j].items() if abs(a) > cutoff}
+        return self
