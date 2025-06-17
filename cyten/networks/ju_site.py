e46360f4eaf0abbc5871385df47f807db314f939
Jakob Unfried <jakob.unfried@tum.de>
2025-06-17 11:04:33 +0200

    Update mockup

--- cyten/networks/ju_site.py
+++ cyten/networks/ju_site.py
@@ -1,122 +1,117 @@
+from __future__ import annotations
 import numpy as np
 from typing import Literal
 
 from ..spaces import ElementarySpace
 from ..tensors import SymmetricTensor
-from ..symmetries import SU2Symmetry, U1Symmetry, ZNSymmetry, NoSymmetry, FibonacciAnyonCategory
+from ..symmetries import SU2Symmetry, U1Symmetry, ZNSymmetry, NoSymmetry, FibonacciAnyonCategory, ProductSymmetry
 
 
-# ------------------------------------------------------------------------------------------
-# Degree of freedom classes : know about local operator algebra
-# ------------------------------------------------------------------------------------------
+# ==========================================================================================
+# Sites: similar role to tenpy v1, but we only have symmetric onsite op
+# ==========================================================================================
 
-
-class DOF:
-    # TODO name choice?? Maybe SpinAlgebra(OperatorAlgebra)?
-    leg: ElementarySpace
-    local_ops: dict[str, SymmetricTensor]  # e.g. MPS.expectation_value can look-up names here
-
-
-class SpinDOF(DOF):
-    def __init__(self, leg: ElementarySpace, spin_vector: np.ndarray, which_symmetry: int = 0):
-        assert spin_vector.shape == (3, leg.dim, leg.dim)
-        self.spin_vector = spin_vector
-        self.spin_symmetry = spin_symmetry = leg.symmetry.factors[which_symmetry]  # dummy: what if symmetry is not a product
-        # more operators?
-        match spin_symmetry:
-            case SU2Symmetry():
-                spin_sector = leg.sector_decomposition[0, which_symmetry]
-                # dummy consider ProdSymmetry.sector_slices, or what if not a ProdSymmetry
-                assert np.all(leg.sector_decomposition[:, which_symmetry] == spin_sector)
-                local_ops = dict()
-            case U1Symmetry():
-                # todo: sanity check that all charge values appear the same # of times
-                local_ops = dict(Sz=spin_vector[-1])
-            case ZNSymmetry(N=2), NoSymmetry():
-                local_ops = dict(Sx=spin_vector[0], Sy=spin_vector[1], Sz=spin_vector[2])
-                # TODO: should we let the DOF always know what 'Sx', ... is?
-                #       that way "forbidden by symmetry" is different error than "misspelled".
-            case _:
-                raise ValueError
-        DOF.__init__(self, leg, local_ops)
-
-
-class FermionDOF(DOF):
-    def __init__(self, leg: ElementarySpace, creator: np.ndarray, annihilator: np.ndarray):
-        self.leg = leg
-        self.creator = creator
-        self.annihilator = annihilator
-        N = creator @ annihilator
-        # ... define more operators?
-        self.number_symmetry = None  # dummy, similar to SpinDOF
-        local_ops = dict(N=N)  # dummy, put more operators
-        DOF.__init__(self, leg, local_ops)
-
-
-class GoldenDOF(DOF):
-    def __init__(self, leg: ElementarySpace, which_symmetry: int = 0):
-        self.fib_symmetry = fib_symmetry = leg.symmetry.factors[which_symmetry]  # dummy: what if symmetry is not a product
-        assert isinstance(fib_symmetry, FibonacciAnyonCategory)
-
-        # dummy: this check needs to be more involved if there are additional symmetries
-        assert leg.sector_multiplicity(fib_symmetry.vacuum) == leg.sector_multiplicity(fib_symmetry.tau)
-
-        DOF.__init__(self, leg)
-
-
-# ------------------------------------------------------------------------------------------
-# Sites: similar role to tenpy v1, but we have the DOF classes instead of local operators.
+# Base class
 # ------------------------------------------------------------------------------------------
 
 
 class Site:
+    state_labels: list[str]
     leg: ElementarySpace
-    degrees_of_freedom: dict[str, DOF]
+    onsite_operators: dict[str, OnSiteOperator]
+    
+    def __init__(self, leg: ElementarySpace,
+                 onsite_operators: dict[str, np.ndarray | SymmetricTensor | OnSiteOperator]
+                 ):
+        ...
 
 
-class SpinSite(Site):
-    spin: float = .5
-    conserve: Literal['SU(2)', 'Sz', 'parity', 'None']
-
-    def __init__(self, spin, *args):
-        d = 2 * spin + 1
-        sx = sy = sz = np.zeros((d, d), complex)  # dummy
-        spin_vector = np.array([sx, sy, sz])
-        leg = ElementarySpace(...)  # dummy: sectors depend on conserve
-        degrees_of_freedom = dict(
-            spin=SpinDOF(leg, spin_vector),
-        )
-        Site.__init__(self, leg=leg, degrees_of_freedom=degrees_of_freedom)
+# Intermediate "interface" classes that promise a certain operator algebra::
+# ------------------------------------------------------------------------------------------
 
 
-class SpinHalfFermionSite(Site):
-    conserve_S: Literal['SU(2)', 'Sz', 'parity', 'None'] = None
-    conserve_N: Literal['N', 'parity', 'None'] = None
+class SpinfulSite(Site):
+    spin_vector: np.ndarray  # [p, p*, i]
+    spin_conserve: Literal['SU(2)', 'Sz', 'parity', 'None']
+    spin_symmetry: SU2Symmetry | U1Symmetry | ZNSymmetry | NoSymmetry
+    # adds e.g. onsite_operators['Sz'] if symmetry allows it and so on
 
-    def __init__(self, *args):
-        leg = ElementarySpace(...)  # dummy: sectors depend on conserve
-        spin_vector = creator = annihilator = np.zeros((4, 4), complex)  # dummy, do actual matrices
-        degrees_of_freedom = dict(
-            spin=SpinDOF(leg, spin_vector),
-            fermion_occupation=FermionDOF(leg, creator, annihilator)
-        )
-        Site.__init__(self, leg=leg, degrees_of_freedom=degrees_of_freedom)
+    def __init__(self,
+                 leg: ElementarySpace,
+                 which_symmetry: int | None,  # such that leg.symmetry[which_symmetry] is the spin symm
+                 spin_conserve: Literal['SU(2)', 'Sz', 'parity', 'None'],
+                 spin_vector: np.ndarray,
+                 onsite_operators: dict[str, np.ndarray | SymmetricTensor | OnSiteOperator]):
+        assert spin_vector.shape == (leg.dim, leg.dim, 3)
+        if spin_conserve != 'SU(2)':
+            self.Sz = spin_vector[:, :, 2]
+        ...
+
+
+class FermionicSite(Site):
+    # TODO similarly for bosons...
+    creators: np.ndarray  # [p, p*, i] where i are different species
+    annihilators: np.ndarray  # [p, p*, i]
+    fermion_species_labels: list[str]  # [i]
+    fermion_conserve: Literal['N', 'parity', 'None']
+    fermion_symmetry: FermionOccupation | FermionParity | NoSymmetry
+
+    def __init__(self,
+                 leg: ElementarySpace,
+                 which_symmetry: int | None,  # such that leg.symmetry[which_symmetry] is the spin symm
+                 fermion_conserve: Literal['N', 'parity', 'None'],
+                 creators: np.ndarray, annihilators: np.ndarray,
+                 fermion_species_labels: list[str],
+                 onsite_operators: dict[str, np.ndarray | SymmetricTensor | OnSiteOperator]
+                 ):
+        self.num_species = Nk = creators.shape[-1]
+        assert creators.shape == (leg.dim, leg.dim, Nk)
+        assert annihilators.shape == (leg.dim, leg.dim, Nk)
+        # N = \sum_k Cd_k C_k    ;   N[p, p*]  = sum_k sum_q Cd[p, q, k] C[q, p*, k]
+        self.fermion_N = np.tensordot(creators, annihilators, ([1, 2], [0, 2]))
+        if fermion_conserve == 'None':
+            raise NotImplementedError  # TODO need to deal with JW strings!
+        ...
+
+
+# Concrete classes::
+# ------------------------------------------------------------------------------------------
+
+
+class SpinSite(SpinfulSite):
+    def __init__(self, spin: float = .5, spin_conserve: Literal['SU(2)', 'Sz', 'parity', 'None'] = 'None'):
+        ...
+
+
+
+# TODO maybe do composition instead of diamond inheritance?
+class SpinHalfFermionSite(SpinfulSite, FermionicSite):
+    def __init__(self,
+                 spin_conserve: Literal['SU(2)', 'Sz', 'parity', 'None'],
+                 fermion_conserve: Literal['N', 'parity', 'None'],
+                 ):
+        ...
+
+
+class SpinLessFermionSite(FermionicSite):
+    def __init__(self,
+                 fermion_conserve: Literal['N', 'parity', 'None'],
+                 ):
+        ...
 
 
 class GoldenSite(Site):
     def __init__(self, handedness):
         symmetry = FibonacciAnyonCategory(handedness)
         leg = ElementarySpace.from_basis(symmetry, [symmetry.vacuum, symmetry.tau])
-        degrees_of_freedom = dict(
-            golden=GoldenDOF(leg)
-        )
-        Site.__init__(self, leg=leg, degrees_of_freedom=degrees_of_freedom)
+        tau_occupation = SymmetricTensor.from_sector_projection([leg], symmetry.tau)
+        Site.__init__(self, leg=leg, onsite_operators={'N': tau_occupation})
 
 
-# ------------------------------------------------------------------------------------------
+# ==========================================================================================
 # Couplings : multi-site operators that are used to build Hamiltonian MPOs,
-# can measure correlation functions etc.
-# ------------------------------------------------------------------------------------------
+#             can measure correlation functions etc.
+# ==========================================================================================
 
 
 class Coupling:
@@ -131,53 +126,68 @@ class Coupling:
 
 
 class OnSiteOperator(Coupling):
-    operator: SymmetricTensor
+    operator: SymmetricTensor  # [p, p*]
 
 
 class HeisenbergCoupling(Coupling):
     """S_i dot S_j"""
-    def __init__(self, site1: Site, site2: Site, dof1='spin', dof2='spin'):
+    def __init__(self, site1: SpinfulSite, site2: SpinfulSite):
+        assert isinstance(site1, SpinfulSite)
+        assert isinstance(site2, SpinfulSite)
         assert site1.leg.symmetry == site2.leg.symmetry
-        s1: SpinDOF = site1.degrees_of_freedom[dof1]
-        s2: SpinDOF = site2.degrees_of_freedom[dof2]
-        h = np.tensordot(s1.spin_vector, s2.spin_vector, (0, 0))  # [p0, p0*, p1, p1*]
+        h = np.tensordot(site1.spin_vector, site2.spin_vector, (0, 0))  # [p0, p0*, p1, p1*]
         h = np.transpose(h, [0, 2, 1, 3])  # [p0, p1, p0*, p1*]
-        Coupling._init_from_numpy(h, [site1, site2])
+        Coupling._init_from_numpy(operator=h, sites=[site1, site2])
 
 
 class ChiralSpinCoupling(Coupling):
     """S_i dot (S_j cross S_k)"""
-    def __init__(self, site1: Site, site2: Site, site3: Site,
-                 dof1='spin', dof2='spin', dof3='spin'):
+    def __init__(self, site1: SpinfulSite, site2: SpinfulSite, site3: SpinfulSite):
+        assert isinstance(site1, SpinfulSite)
+        assert isinstance(site2, SpinfulSite)
+        assert isinstance(site3, SpinfulSite)
         assert site1.leg.symmetry == site2.leg.symmetry == site3.leg.symmetry
-        s1: SpinDOF = site1.degrees_of_freedom[dof1]
-        s2: SpinDOF = site2.degrees_of_freedom[dof2]
-        s3: SpinDOF = site3.degrees_of_freedom[dof3]
-
-        # dummy: should be cross product
-        S2_cross_S3 = s2.spin_vector[:, :, None, :, None] * s3.spin_vector[:, None, :, None, :]
-
-        h = np.tensordot(s1.spin_vector, S2_cross_S3, (0, 0))
+        S2_cross_S3 = np.cross(site1.spin_vector, site2.spin_vector, axis=-1)
+        h = np.tensordot(site1.spin_vector, S2_cross_S3, (0, 0))
         h = np.transpose(h, [0, 2, 1, 3, 4, 5])  # dummy: leg order is wrong
-        Coupling._init_from_numpy(h, [site1, site2, site3])
+        Coupling._init_from_numpy(operator=h, sites=[site1, site2, site3])
 
 
 class GoldenCoupling(Coupling):
     """projects two sites onto the tau fusion channel"""
-    def __init__(self, site1: Site, site2: Site, dof1='golden', dof2='golden'):
+    def __init__(self, site1: GoldenSite, site2: GoldenSite):
+        assert isinstance(site1, GoldenSite)
+        assert isinstance(site2, GoldenSite)
         assert site1.leg.symmetry == site2.leg.symmetry
         tau_sector = [1]
 
         # this classmethod doesnt exist yet, would be light wrapper around from_sector_block_func
         h = SymmetricTensor.from_sector_projection([site1.leg, site2.leg], tau_sector)
 
-        # in this case, we dont actually need the DOF, correct? the symmetry is already enough.
         Coupling._init_from_tensor(h)
 
 
-# ------------------------------------------------------------------------------------------
+class FermionHopping(Coupling):
+    """sum_k c_{i,k}^\dagger c_{j,k}
+
+    i.e. hopping of all fermion species with same amplitude.
+
+    TODO generalize to hopping of individual species here, by giving prefactors, or in new
+    class(es)?
+    """
+    def __init__(self, site1: FermionicSite, site2: FermionicSite, plus_hc: bool = True):
+        assert isinstance(site1, FermionicSite)
+        assert isinstance(site2, FermionicSite)
+        h = np.tensordot(site1.creators, site2.annihilators, (-1, -1))  # [p1, p1*, p2, p2*]
+        h = np.transpose(h, [0, 2, 1, 3])
+        if plus_hc:
+            h = h + h.conj().transpose([2, 3, 0, 1])
+        Coupling._init_from_tensor(h)
+
+
+# ==========================================================================================
 # Helpers for name lookups
-# ------------------------------------------------------------------------------------------
+# ==========================================================================================
 
 
 coupling_aliases = {
@@ -185,8 +195,9 @@ coupling_aliases = {
     ChiralSpinCoupling: ['S.SxS'],
     ...: ['S+S-'],
     ...: ['SzSz'],
-    ...: ['fermion_hop', 'CdC'],
+    FermionHopping: ['fermion_hop', 'CdC'],  # todo establish rules for a mini-language?
     ...: ['boson_hop', 'BdB'],
+    ...: ...,
 }
 
 
@@ -210,6 +221,7 @@ def add_coupling_alias(cls: type[Coupling], alias: str, *more: str):
 
 # used e.g. when building MPOs in Hamiltonians, or in MPS.correlation_function
 def get_coupling(cls: str | type[Coupling], sites: list[Site]):
+    # TODO think about caching here
     if isinstance(cls, str):
         cls = coupling_aliases_lookup.get(cls, None)
         if cls is None:
