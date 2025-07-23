1362f94d3af31ba5d3759f2b0b3e5efb7bc56275
nikirchner <126802899+nikirchner@users.noreply.github.com>
2025-07-23 14:17:13 +0200

    add ClockSite

--- cyten/models/__init__.py
+++ cyten/models/__init__.py
@@ -4,7 +4,7 @@
 # TODO update this!
 from .sites import (
     Site, SpinfulSite, FermionicSite, BosonicSite, SpinSite, SpinHalfFermionSite,
-    GoldenSite
+    ClockSite, GoldenSite
 )
 from .couplings import (
     Coupling, OnSiteOperator, heisenberg_coupling, chiral_3spin_coupling, gold_coupling
