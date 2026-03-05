bd87b0d5ebbfb09e59b73c4b20eeb539bba0abf8
Johannes Hauschild <git@johannes-hauschild.de>
2026-03-05 11:52:04 +0100

    remove Python wrapper of NumpyBlockBackend

--- cyten/block_backends/__init__.py
+++ cyten/block_backends/__init__.py
@@ -4,7 +4,7 @@
 # Note: order matters to avoid circular imports!
 # pyright: ignore
 from .._core import Dtype  # noqa
-from .._core import NumpyBlock, NumpyBlockBackend as _NumpyBlockBackendCpp
+from .._core import NumpyBlock, NumpyBlockBackend
 from . import dtypes
 from .dtypes import _DtypeEnumWrapper
 
@@ -13,42 +13,6 @@ from .array_api import ArrayApiBlockBackend
 from .torch import TorchBlockBackend
 
 import numpy as np
-
-
-class NumpyBlockBackend(_NumpyBlockBackendCpp):
-    """Numpy block backend (C++ implementation)."""
-
-    BlockCls = (np.ndarray, NumpyBlock)
-
-    def permute_combined_matrix(self, block, dims1, idcs1, dims2, idcs2):
-        dims1, idcs1, dims2, idcs2 = list(dims1), list(idcs1), list(dims2), list(idcs2)
-        input_was_ndarray = isinstance(block, np.ndarray)
-        if isinstance(block, np.ndarray):
-            block = _NumpyBlockBackendCpp.as_block(self, block, None, None)
-        res = _NumpyBlockBackendCpp.permute_combined_matrix(self, block, dims1, idcs1, dims2, idcs2)
-        return res.array() if input_was_ndarray else res
-
-    def permute_combined_idx(self, block, axis, dims, idcs):
-        dims, idcs = list(dims), list(idcs)
-        input_was_ndarray = isinstance(block, np.ndarray)
-        if isinstance(block, np.ndarray):
-            block = _NumpyBlockBackendCpp.as_block(self, block, None, None)
-        res = _NumpyBlockBackendCpp.permute_combined_idx(self, block, axis, dims, idcs)
-        return res.array() if input_was_ndarray else res
-
-    def test_block_sanity(
-        self,
-        block,
-        expect_shape=None,
-        expect_dtype=None,
-        expect_device=None,
-    ):
-        if not isinstance(block, self.BlockCls):
-            raise AssertionError('wrong block type')
-        # Convert ndarray to NumpyBlock so C++ can validate (C++ throws RuntimeError on failure)
-        if isinstance(block, np.ndarray):
-            block = _NumpyBlockBackendCpp.as_block(self, block, None, None)
-        _NumpyBlockBackendCpp.test_block_sanity(self, block, expect_shape, expect_dtype, expect_device)
 
 
 dtypes.Dtype = Dtype
