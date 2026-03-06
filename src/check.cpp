5d75baa294834ac1d5e3b8e0cb8a813927a1d697
Johannes Hauschild <git@johannes-hauschild.de>
2026-03-06 15:50:02 +0100

    add python test runner script to simplify setup of debugging with C++.

--- src/check.cpp
+++ src/check.cpp
@@ -2,13 +2,18 @@
 #include "check.h"
 #include "cyten/cyten.h"
 
-#include <string>
+#include <iostream>
 
 namespace cyten {
 
 int64
 add(int64 i, int64 j)
 {
+    // if (j < 0)
+    // {
+    //     std::cout << "j is negative, testing C++ exception handling" << std::endl;
+    //     throw std::runtime_error("test exception from cyten/check.cpp:add");
+    // }
     return i + j;
 }
 
--- tests/python_tests/test_check.py
+++ tests/python_tests/test_check.py
@@ -1,3 +1,5 @@
+import pytest
+
 import cyten
 
 
@@ -5,3 +7,13 @@ def test_check():
     x = cyten.add(1, 1)
     assert x == 2
     assert cyten.add(1, -1) == 0
+
+
+def test_check_exception():
+    with pytest.raises(RuntimeError):
+        cyten.add(1, -1)
+
+
+if __name__ == '__main__':
+    test_check_exception()
+    test_check()
